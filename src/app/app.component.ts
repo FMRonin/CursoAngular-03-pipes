aab5493be5f7fb33ef0974d0d644dada53716eed
Fabian Martinez <fabianemartinez@gmail.com>
2019-01-05 09:44:07 -0500

    pipes1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { resolve } from 'url';
+import { reject } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  nombre = 'Fabian';
+
+  arreglo = [0,1,2,3,4,5];
+
+  PI = Math.PI;
+
+  PI2 = 3.1415;
+
+  a = 0.234;
+
+  current = 1200.12;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolvwrine",
+    edad: 300,
+    direccion:{
+      casa: 2,
+      telefono: '1234566-123'
+    }
+  }
+
+  valorDePromesa = new Promise((resolve, reject) => {
+    setTimeout(()=>resolve('Llego la data'),3500);
+  })
 }
